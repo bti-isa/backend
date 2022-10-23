65b793ec0603fea23e1f08d177cc85c917c2caad
Vuk Milanovic <vuk.milanovic11@gmial.com>
2022-10-23 17:49:56 +0200

    Project initialization

---
+++ src/main/java/com/isa/bti/BtiApplication.java
@@ -0,0 +1,13 @@
+package com.isa.bti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isa/bti/BtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa.bti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
