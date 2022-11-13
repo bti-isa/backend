d6dc6056657961679efd0e27c451ffb876944299
Vuk Milanovic <vuk.milanovic11@gmial.com>
2022-11-13 22:33:42 +0100

    Transactional annotation added

--- src/main/java/com/isa/BloodTransferInstitute/repository/AddressRepository.java
+++ src/main/java/com/isa/BloodTransferInstitute/repository/AddressRepository.java
@@ -3,6 +3,8 @@ package com.isa.BloodTransferInstitute.repository;
 import com.isa.BloodTransferInstitute.model.Address;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
--- src/main/java/com/isa/BloodTransferInstitute/repository/LocationRepository.java
+++ src/main/java/com/isa/BloodTransferInstitute/repository/LocationRepository.java
@@ -3,6 +3,8 @@ package com.isa.BloodTransferInstitute.repository;
 import com.isa.BloodTransferInstitute.model.Location;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
 }
