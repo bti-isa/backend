73fc81d3d00e49f21f4eae090fd324a7a77e0a08
Vuk Milanovic <vuk.milanovic11@gmial.com>
2022-11-21 16:26:07 +0100

    Exception handler added

--- src/main/java/com/isa/BloodTransferInstitute/exception/ScheduleException.java
+++ src/main/java/com/isa/BloodTransferInstitute/exception/ScheduleException.java
@@ -1,7 +1,9 @@
 package com.isa.BloodTransferInstitute.exception;
 
-public class ScheduleException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+
+public class ScheduleException extends BaseException {
 	public ScheduleException() {
-		super("The patient had an examination within the last six months or the patient already has scheduled examination.");
+		super("The patient had an examination within the last six months or the patient already has scheduled examination.", HttpStatus.BAD_REQUEST, HttpStatus.BAD_REQUEST.value());
 	}
 }
