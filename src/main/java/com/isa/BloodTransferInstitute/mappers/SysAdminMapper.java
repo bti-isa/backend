348f93de7ad45998198dc12588f640a3e3d14461
Nikola Grbovic <grbovicn.ra@gmail.com>
2022-12-20 15:28:35 +0100

    feature: add new system admin

---
+++ src/main/java/com/isa/BloodTransferInstitute/mappers/SysAdminMapper.java
@@ -0,0 +1,41 @@
+package com.isa.BloodTransferInstitute.mappers;
+
+import com.isa.BloodTransferInstitute.dto.user.sysadmin.NewSysAdminDTO;
+import com.isa.BloodTransferInstitute.dto.user.sysadmin.SysAdminDTO;
+import com.isa.BloodTransferInstitute.enums.Role;
+import com.isa.BloodTransferInstitute.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SysAdminMapper {
+    public User DTOtoEntity(NewSysAdminDTO dto) {
+        final var updatedUser = User.builder()
+                .deleted(false)
+                .firstname(dto.getFirstname())
+                .lastname(dto.getLastname())
+                .email(dto.getEmail())
+                .password(dto.getPassword())
+                .phoneNumber(dto.getPhoneNumber())
+                .jmbg(dto.getJmbg())
+                .gender(dto.getGender())
+                .role(Role.SYSTEM_ADMIN)
+                .accountActivated(false)
+                .build();
+        return updatedUser;
+    }
+    public SysAdminDTO EntityToDTO(User admin) {
+        final var dto = SysAdminDTO.builder()
+                .id(admin.getId())
+                .firstname(admin.getFirstname())
+                .lastname(admin.getLastname())
+                .email(admin.getEmail())
+                .jmbg(admin.getJmbg())
+                .gender(admin.getGender())
+                .role(admin.getRole())
+                .phoneNumber(admin.getPhoneNumber())
+                .build();
+        return dto;
+    }
+}
