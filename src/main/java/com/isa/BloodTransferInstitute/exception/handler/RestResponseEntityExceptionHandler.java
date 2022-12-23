b2f3b88e0c20fc31797fdc3bb68ba19c65f5a10f
unknown <kalinic.ra71.2019@uns.ac.rs>
2022-12-23 10:47:22 +0100

    bug fix about registration

--- src/main/java/com/isa/BloodTransferInstitute/exception/handler/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/isa/BloodTransferInstitute/exception/handler/RestResponseEntityExceptionHandler.java
@@ -1,11 +1,8 @@
 package com.isa.BloodTransferInstitute.exception.handler;
 
-import com.isa.BloodTransferInstitute.exception.BaseException;
-import com.isa.BloodTransferInstitute.exception.CreateAppointmentException;
-import com.isa.BloodTransferInstitute.exception.NotFoundException;
-import com.isa.BloodTransferInstitute.exception.ScheduleException;
-import com.isa.BloodTransferInstitute.exception.UserHasNoRoleException;
+import com.isa.BloodTransferInstitute.exception.*;
 
+import org.hibernate.procedure.ParameterStrategyException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +15,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@ExceptionHandler(value = { NotFoundException.class, CreateAppointmentException.class, ScheduleException.class, UserHasNoRoleException.class, UsernameNotFoundException.class })
+	@ExceptionHandler(value = { NotFoundException.class, CreateAppointmentException.class, ScheduleException.class, UserHasNoRoleException.class, UsernameNotFoundException.class, ParameterStrategyException.class, CancelException.class })
 	protected ResponseEntity<Object> handleException(BaseException ex, WebRequest request) {
 		String bodyOfResponse = ex.getMessage();
 		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), ex.getStatus(), request);
