832075a001bfa2be9b3544063598fdc2a4afe7da
unknown <kalinic.ra71.2019@uns.ac.rs>
2023-01-25 17:38:08 +0100

    read data from queue and push on websocket

---
+++ src/main/java/com/isa/BloodTransferInstitute/controller/MapController.java
@@ -0,0 +1,38 @@
+package com.isa.BloodTransferInstitute.controller;
+
+import com.isa.BloodTransferInstitute.dto.CordDTO;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/map")
+@RequiredArgsConstructor
+public class MapController {
+    private final SimpMessagingTemplate simpMessagingTemplate;
+    @Autowired
+    RabbitListenerEndpointRegistry registry;
+
+    @PostMapping("/start")
+    public String StartDelivery(@RequestBody CordDTO cordDto) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8085/start";
+        restTemplate.postForObject(url, cordDto, Object.class);
+        registry.getListenerContainer("foo").start();
+        return "";
+    }
+    
+    @RabbitListener(id = "foo", queues = "test", autoStartup = "false")
+    public void consumeMessageFromQueue (String cord){
+        System.out.println("Message recieved from queue: " + cord);
+        this.simpMessagingTemplate.convertAndSend("/socket-publisher", cord);
+    }
+}
